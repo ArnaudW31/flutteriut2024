0890dc75f72cda66e0402a093debd2cbbcbbae79
Paul Beaurepaire <beaurepairepaul@gmail.com>
2024-03-26 14:06:31 +0100

    Fix merge

--- lib/app/weatherApp.dart
+++ lib/app/weatherApp.dart
@@ -10,12 +10,7 @@ class weatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title : "Météo",
-        theme: ThemeData(
-          appBarTheme : const AppBarTheme(color: Colors.greenAccent),
-        ),
-        home : const WeatherRoot()
-      title: "Météo",
+        title: "Météo",
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Colors.greenAccent),
       ),
